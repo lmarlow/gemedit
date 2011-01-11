1cbe40b21866c940b82d3f9ca8f6878bc74b3340
Lee Marlow <lee.marlow@gmail.com>
2011-01-10 23:52:45 -0700

    Unset all editor env vars for testing

--- features/step_definitions/env_steps.rb
+++ features/step_definitions/env_steps.rb
@@ -1,4 +1,7 @@
 Then /\bset the environment variables with "([^"]*)"$/ do | env |
+  %w( GEMEDITOR VISUAL EDITOR ).each do | key |
+    set_env(key, nil)
+  end
   env.scan(/(\w+)=(\w+)/).each do | key, value |
     set_env(key, value)
   end
