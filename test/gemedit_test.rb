f02a509ce2771c0d536425aa37482deb3b46fdc5
Lee Marlow <lee.marlow@gmail.com>
2011-09-28 01:54:34 -0600

    Bump version to 1.0.1
    Add a history entry and link to the github contributors page
    [Fixes #5 and #8]

--- test/gemedit_test.rb
+++ test/gemedit_test.rb
@@ -4,6 +4,6 @@ require 'gemedit'
 
 class GemeditTest < Test::Unit::TestCase
   def test_gemedit_version
-    assert_equal '1.0.0', Gemedit::Version
+    assert_equal '1.0.1', Gemedit::Version
   end
 end
